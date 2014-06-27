e4fbb3ac3adc3675cf69aba278f5d0ecb7ec3de6
cat <sbchou@gmail.com>
2014-06-27 17:10:02 -0400

    add the Hollow Men ts eliot

--- README.md
+++
@@ -1,4 +0,0 @@
-codepoems by sophie
-code as poems that output poems. 
-inspired by ishac bertran: http://ishback.com/work/codepoems/codepoems.html
-in progress.
---
+++ theHollowMen.py
@@ -0,0 +1,26 @@
+"""
+T.S. Eliot, 1925
+cat, 2014.06.27
+"""
+
+class World:
+    def __str__(self):
+        return "this is the way the world ends"
+
+    def ends(self):
+        return ["whimper"]
+
+def main():
+    world = World()
+
+    for i in range(4):
+        print world
+
+    if "bang" not in world.ends():
+        print "not with a bang"
+
+    if "whimper" in world.ends():
+        print "but with a whimper"
+
+if __name__ == "__main__":
+    main()
